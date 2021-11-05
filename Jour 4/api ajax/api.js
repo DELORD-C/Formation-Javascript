6f881b8db7576f3ebe031c74a96dc2907e844383
Clément <clement.delord@epitech.eu>
2021-11-05 12:24:58 +0100

    Commentaires explicatifs

--- Jour 4/api ajax/api.js
+++ Jour 4/api ajax/api.js
@@ -3,19 +3,19 @@ let champ = document.getElementById('text'),
 
 champ.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
-        let requete = new XMLHttpRequest();
-        requete.onreadystatechange = function () {
-            if (requete.readyState === 4) {
-                let resultats = JSON.parse(requete.responseText);
+        let requete = new XMLHttpRequest(); //creation de la requète
+        requete.onreadystatechange = function () { //ici on affecte une fonction au changement d'état de la requète
+            if (requete.readyState === 4) { //on vérifie que la requete est terminée (etat n°4)
+                let resultats = JSON.parse(requete.responseText); //on récupère le résultat de la requète
                 resultats = resultats.entries;
-                resultats.forEach(resultat => {
-                    let newElem = document.createElement('li');
-                    newElem.innerHTML = resultat.Link;
-                    liste.append(newElem);
+                resultats.forEach(resultat => { //pour chaque résultat :
+                    let newElem = document.createElement('li'); //on créer un élément <li>
+                    newElem.innerHTML = resultat.Link; //on place le résultat dans notre élément <li>
+                    liste.append(newElem); //on ajoute notre élément <li> à notre <div> liste
                 });
             }
         }
-        requete.open('GET', 'http://api.publicapis.org/entries?category=' + champ.value);
-        requete.send();
+        requete.open('GET', 'http://api.publicapis.org/entries?category=' + champ.value); //définition de l'url cible
+        requete.send(); //Lancement de la requète
     }
 });
--- Jour 4/verif.js
+++ Jour 4/verif.js
@@ -1,3 +1,4 @@
+//On commence par récupérer tous nos champs et on les stocke dans des variables
 let nom = document.getElementById('Nom'),
     prenom = document.getElementById('Prenom'),
     email = document.getElementById('Email'),
@@ -5,45 +6,45 @@ let nom = document.getElementById('Nom'),
     tel = document.getElementById('Tel'),
     formulaire = document.getElementById('formulaire');
 
-function verifForm (event) {
-    event.preventDefault();
-    let erreurs = 0;
-    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
-        email.style.border = '2px solid red';
-        erreurs++;
+function verifForm (event) { //notre fonction de vérification
+    event.preventDefault(); //on empèche le formulaire de s'envoyer et donc de rafraichir / changer de page
+    let erreurs = 0; //on créé une variable tampon qui va s'incrémenter à chaque erreur du formulaire
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) { //on test le champ email avec une regex
+        email.style.border = '2px solid red'; //si le champ est faux, on ajoute une bordure rouge
+        erreurs++; //et on incrémente la variable erreurs
     }
     else {
-        email.style.border = '1px solid black';
+        email.style.border = '1px solid black'; //sinon, on remet la bordure à son état d'origine
     }
-    if (!/^\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$/.test(mdp.value)) {
+    if (!/^\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$/.test(mdp.value)) { //on test le champ mot de passe avec une regex
         mdp.style.border = '2px solid red';
         erreurs++;
     }
     else {
         mdp.style.border = '1px solid black';
     }
-    if (!/(0|\\+33|0033)[1-9][0-9]{8}/.test(tel.value)) {
+    if (!/(0|\\+33|0033)[1-9][0-9]{8}/.test(tel.value)) { //on test le champ téléphone avec une regex
         tel.style.border = '2px solid red';
         erreurs++;
     }
     else {
         tel.style.border = '1px solid black';
     }
-    if (!/^([A-z]|-| ){2}([A-z]|-| )+$/.test(nom.value)) {
+    if (!/^([A-z]|-| ){2}([A-z]|-| )+$/.test(nom.value)) { //on test le champ nom avec une regex
         nom.style.border = '2px solid red';
         erreurs++;
     }
     else {
         nom.style.border = '1px solid black';
     }
-    if (!/^([A-z]|-| ){2}([A-z]|-| )+$/.test(prenom.value)) {
+    if (!/^([A-z]|-| ){2}([A-z]|-| )+$/.test(prenom.value)) { //on test le champ prenom avec une regex
         prenom.style.border = '2px solid red';
         erreurs++;
     }
     else {
         tel.style.border = '1px solid black';
     }
-    if (erreurs === 0) {
-        formulaire.submit();
+    if (erreurs === 0) { //si il n'y a eu aucune erreur :
+        formulaire.submit(); //on envoi le formulaire
     }
 }
