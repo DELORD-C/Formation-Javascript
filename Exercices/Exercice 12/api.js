65c0c292ac88e88d25cbdd2fba3ff00167e1a8ce
Clément <clement.delord@epitech.eu>
2021-11-05 12:13:57 +0100

    Correction exercice 12

--- Exercices/Exercice 12/api.js
+++ Exercices/Exercice 12/api.js
@@ -1,21 +1,18 @@
-let champ = document.getElementById('btn'),
-    liste = ?;
+let liste = document.getElementById('liste');
 
 function apiCall () {
-    if (event.key === 'Enter') {
-        let requete = new XMLHttpRequest();
-        requete.onreadystatechange = function () {
-            if (requete.readyState === 4) {
-                let resultats = JSON.parse(requete.responseText);
-                resultats = resultats.entries;
-                resultats.forEach(resultat => {
-                    let newElem = document.createElement('p');
-                    newElem.innerHTML = resultat.Link;
-                    liste.append(newElem);
-                });
-            }
+    let requete = new XMLHttpRequest();
+    requete.onreadystatechange = function () {
+        if (requete.readyState === 4) {
+            let resultats = JSON.parse(requete.responseText);
+            console.log(resultats);
+            resultats.forEach(resultat => {
+                let newElem = document.createElement('p');
+                newElem.innerHTML = resultat;
+                liste.append(newElem);
+            });
         }
-        requete.open('GET', '?' + champ.value);
-        requete.send();
     }
+    requete.open('GET', 'http://api.publicapis.org/categories');
+    requete.send();
 }
